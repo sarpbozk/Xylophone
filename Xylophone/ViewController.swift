dc633d24bdc164d071403def581818ca9a2bed01
Sarp Bozkurt <sarpbozk@gmail.com>
2024-03-06 23:09:30 +0300

    initial commit

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,33 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    var player: AVAudioPlayer?
+    func playSound(_ soundLetter: String) {
+        guard let path = Bundle.main.path(forResource: soundLetter, ofType:"wav") else {
+            return }
+        let url = URL(fileURLWithPath: path)
 
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
-    
-
+    @IBAction func noteButtonPressed(_ sender: UIButton) {
+        playSound(sender.titleLabel?.text ?? "")
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1
+        }
+    }
 }
 
